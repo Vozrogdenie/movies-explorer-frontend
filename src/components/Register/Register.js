f5d799cba75f702d083e51c42ed737f5e5381e40
Vozrogdenie <93585141+Vozrogdenie@users.noreply.github.com>
2023-03-16 00:38:39 +0300

    валидация регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import headLogo from '../../images/logo.png'
 import api from '../../utils/auth';
 import { useNavigate } from 'react-router-dom';
-
+import React from 'react';
+import useFormWithValidation from '../hooks/useFormWithValidation';
 function Register(props) {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [emailDirty, setEmailDirty] = useState(false);
+    // const [passwordDirty, setPasswordDirty] = useState(false);
+    // const [emailError, setEmailError] = useState('Поле не может быть пустым');
+    // const [passwordError, setPasswordError] = useState('Поле не может быть пустым');
     const history = useNavigate()
-    const onNameChange = (e) => {
-        setName(e.target.value);
-    };
-    const onEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+    const {
+        values, errors, isValid, handleChange,
+      } = useFormWithValidation();
+    
+      const { name, email, password } = values;
 
-    const onRegister = (e) => {
+      const onRegister = (e) => {
         e.preventDefault();
-        
-        api.register(name, email, password);
+        if (isValid) {
+            api.register(name, email, password);
         history('/movies')
+          }else{
+            console.log()
+          }
+       
     };
 
     return(
@@ -30,18 +35,18 @@ function Register(props) {
             {props.children}
             <a href="/" className='register__logo'><img src={headLogo} alt="Логотип"/></a>
             <h3 className="register__welcom">Добро пожаловать!</h3>
-            <form  className="register__form">
+            <form onClick={onRegister} className="register__form">
                 <label id="name" name="name" className="register__label" >Имя</label>
-                <input onChange={onNameChange} required type='text' minLength="2" className="register__input"  placeholder="Имя"></input>
+                <input  required type='text' minLength="2" className="register__input"  placeholder="Имя"></input>
                 <span className="register__error">Поле должно быть заполнено. Минимум 2 буквы</span>
                 <label className="register__label">E-mail</label>
-                <input onChange={onEmailChange} required  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" id="email" name="email" type="text" placeholder="Email" className="register__input"></input>
+                <input onChange={handleChange} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" value={email || ''} required   id="email" name="email" type="text" placeholder="Email" className="register__input"></input>
                 <span className="register__error">Это поле должно содержать E-Mail в формате example@site.com</span>
                 <label id="password" className="register__label">Пароль</label>
-                <input onChange={onPasswordChange} required className="register__input" type="password" name="password" minLength="4" placeholder="Пароль" maxLength="20"></input>
-                <span className="register__error">Поле должно быть заполнено. Минимум 4 символа и максимум 20.</span>
+                <input  value={password} required className="register__input" type="password" name="password"  minLength="4" placeholder="Пароль" maxLength="20" ></input>
+                <span className="register__error">{errors.password}Поле должно быть заполнено. Минимум 4 символа и максимум 20.</span>
             </form>
-            <button onClick={onRegister} className="register__register">Зарегистрироваться</button>
+            <button onClick={onRegister} disabled={!isValid} className="register__register">Зарегистрироваться</button>
             <p className="register__title">Уже зарегистрированы?&nbsp; <a href="/signin"> Войти</a></p>
         </div>
     )
